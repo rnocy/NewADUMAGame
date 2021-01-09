8e1aaae2837c9a5c136cde6b3befa536c6cccca9
rnocy <77068538+rnocy@users.noreply.github.com>
2021-01-09 15:52:39 +0700

    Bug between level 1-2
    Cannot linked script for Timer2 (lv2) and Typer2(lv2)

---
+++ ADUMA070121.2/Assets/Scripts/Timer2.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer2 : MonoBehaviour
+{
+    Image timerBar;
+    public float maxTime = 5f;
+    float timeLeft;
+    public GameObject timesUpText;
+    public TextMeshProUGUI wordCountText;
+    public GameObject retryButton;
+    public Typer typer;
+    public static bool boo_stop;
+
+    void Start()
+    {
+        timesUpText.SetActive(false);
+        retryButton.gameObject.SetActive(false);
+        wordCountText.gameObject.SetActive(false);
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+    }
+    void Update()
+    {
+        if (!boo_stop)
+        {
+            if (timeLeft >= 0)
+            {
+                timeLeft -= Time.deltaTime;
+                timerBar.fillAmount = timeLeft / maxTime;
+
+            }
+            else
+            {
+                timesUpText.SetActive(true);
+                Typer2.boo_gameOver = true;
+                wordCountText.text = "Words left: " + typer.int_wordCount;
+                wordCountText.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                retryButton.gameObject.SetActive(true);
+            }
+        }
+    }
+    public void fn_time(bool _boo_add)
+    {
+        timeLeft += _boo_add ? 1f : -2f;
+
+    }
+}
---
+++ ADUMA070121.2/Assets/Scripts/Typer2.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Typer2 : MonoBehaviour
+{
+    public Text wordOutput = null;
+    private string remainingWord = string.Empty;
+    private string currentWord = "muffins";
+    public string[] arrStr_bank;
+    public static bool boo_gameOver;
+    public int int_wordCount;
+    public TextMeshProUGUI winText;
+    int int_correct;
+    public Timer scr_time;
+    public GameObject nextButton;
+
+
+    private void Awake()
+    {
+        winText.gameObject.SetActive(false);
+        nextButton.gameObject.SetActive(false);
+    }
+
+    public void fn_init()
+    {
+        SetCurrentWord();
+    }
+    void fn_pick()
+    {
+
+    }
+    private void SetCurrentWord()
+    {
+        //Use wordbank here.
+        //SetRemainingWord(currentWord);\
+        if (int_wordCount > 0)
+        {
+            currentWord = arrStr_bank[Random.Range(0, arrStr_bank.Length)];
+            SetRemainingWord(currentWord);
+            int_wordCount -= 1;
+        }
+        else
+        {
+            Timer.boo_stop = true;
+            Debug.Log("Win");
+            winText.gameObject.SetActive(true);
+            nextButton.gameObject.SetActive(true);
+            //wining condition
+        }
+
+    }
+    private void SetRemainingWord(string newString)
+    {
+        remainingWord = newString;
+        wordOutput.text = remainingWord;
+    }
+
+    void Update()
+    {
+        if (!boo_gameOver) CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.anyKeyDown)
+        {
+            string keysPressed = Input.inputString;
+
+            if (keysPressed.Length == 1)
+                EnterLetter(keysPressed);
+        }
+    }
+
+    private void EnterLetter(string typedLetter)
+    {
+        if (IsCorrectLetter(typedLetter))
+        {
+            int_correct += 1;
+            RemoveLetter();
+
+            if (IsWordComplete())
+                SetCurrentWord();
+        }
+        else int_correct -= 1;
+        if (Mathf.Abs(int_correct) == 3)
+        {
+            scr_time.fn_time(int_correct > 0);
+            ///add UI for combo announcement
+        }
+    }
+
+    private bool IsCorrectLetter(string letter)
+    {
+        return remainingWord.IndexOf(letter) == 0;
+    }
+
+    private void RemoveLetter()
+    {
+        string newString = remainingWord.Remove(0, 1);
+        SetRemainingWord(newString);
+    }
+
+    private bool IsWordComplete()
+    {
+        return remainingWord.Length == 0;
+    }
+
+}
