6bffa3460ffb5ac8ae3a40119450a2306ea09f71
rnocy <77068538+rnocy@users.noreply.github.com>
2021-01-18 23:58:17 +0700

    Fixe Transition
    Fix transition TO PLOT

---
+++ ADUMA070121.2/Assets/Scripts/Menu/ToPlotScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToPlotScene : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    void Start()
+    {
+        transition.gameObject.SetActive(false);
+    }
+
+    public void Next()
+    {
+
+        StartCoroutine(loadPlot("PlotScene"));
+
+    }
+
+
+
+    IEnumerator loadPlot(string levelIndex)
+    {
+        transition.SetTrigger("Start");
+        transition.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(levelIndex);
+
+    }
+}
