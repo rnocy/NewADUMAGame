cb161b64db834c9d9549ab5bb4b8e7b61b5a333f
rnocy <77068538+rnocy@users.noreply.github.com>
2021-01-21 15:46:56 +0700

    Transition & Music
    added bg music

---
+++ ADUMA070121.2/Assets/Scripts/Menu/ToLevel1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLevel1 : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    void Start()
+    {
+        transition.gameObject.SetActive(false);
+    }
+
+    public void Next()
+    {
+
+        StartCoroutine(loadLevelOne("FirstLevel"));
+
+    }
+
+
+
+    IEnumerator loadLevelOne(string levelIndex)
+    {
+        transition.SetTrigger("Start");
+        transition.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(levelIndex);
+
+    }
+}
