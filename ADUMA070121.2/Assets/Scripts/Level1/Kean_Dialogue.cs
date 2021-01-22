3b70a35863f75baddc0c483c2f67b8304c5f1141
rnocy <77068538+rnocy@users.noreply.github.com>
2021-01-22 11:32:08 +0700

    update UI tutorial

---
+++ ADUMA070121.2/Assets/Scripts/Level1/Kean_Dialogue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Kean_Dialogue : MonoBehaviour
+{
+    public string name;
+    public string sentences;
+
+}
