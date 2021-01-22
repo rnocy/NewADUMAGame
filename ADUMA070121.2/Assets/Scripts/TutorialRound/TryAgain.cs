1172a70c40979427a3fb761a039b6afba9556525
rnocy <77068538+rnocy@users.noreply.github.com>
2021-01-22 17:33:14 +0700

    Still the same
    Vẫn vậy nhưng mà sửa lại xíu UI

---
+++ ADUMA070121.2/Assets/Scripts/TutorialRound/TryAgain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TryAgain : MonoBehaviour
+{
+ 
+        public void RePlay()
+        {
+            SceneManager.LoadScene("TutorialRound");
+        }
+
+
+}
